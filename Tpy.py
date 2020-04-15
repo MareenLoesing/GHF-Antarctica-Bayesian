9e66054b2267ff91533e87d71969f3ffe48070a5
MareenLoesing <63237282+MareenLoesing@users.noreply.github.com>
2020-04-15 14:11:07 +0200

    Add files via upload

---
+++ Tpy.py
@@ -0,0 +1,100 @@
+import sys
+import numpy as np
+sys.path.append("Y:")
+import wopy.MCMC.toy as toy
+import matplotlib.pyplot as plt
+plt.ion()
+import seaborn as sns
+import pandas as pd
+
+class TempCalc:
+    def __init__(self,moho,isotherm_depths,surface_temp,Heat_prod=None):
+        self.isotherm_depths = isotherm_depths
+        self.moho = moho
+        self.T0 = surface_temp
+        if not Heat_prod is None: self.A = Heat_prod
+        else: self.A = 0        
+    def __call__(self,args):
+        args = np.asarray(args)
+        '''
+        k1 = crustal therm. condtuctivity
+        k2 = mantle therm. conduct.
+        A = crustal heat production
+        qD = heatflux from mantle to crust
+        '''
+        if (self.A == 0):
+            k1,k2,qD,A = args.T           
+        else:
+            k1,k2,qD = args.T
+            A = self.A
+        T = np.zeros(k1.shape+(len(self.isotherm_depths),))
+        moho = self.moho 
+        for i,z in enumerate(self.isotherm_depths):
+            if z<=moho:
+                T[...,i]=self.T0+(qD+A*moho)/k1*z-A/(2.0*k1)*z**2
+            else:
+                T[...,i]=self.T0+moho*(qD/k1+0.5*A*moho/k1-qD/k2)+qD/k2*z
+        return T
+
+
+
+class TempCalcExp:
+    def __init__(self,moho,isotherm_depths,surface_temp,Heat_prod=None):
+        self.isotherm_depths = isotherm_depths
+        self.moho = moho
+        self.T0 = surface_temp
+        if not Heat_prod is None: self.A = Heat_prod
+        else: self.A = 0        
+    def __call__(self,args):
+        args = np.asarray(args)
+        if (self.A == 0):
+            k1,k2,qD,A = args.T           
+        else:
+            k1,k2,qD = args.T
+            A = self.A
+        H0 = A #surface heat production
+        hr = 8000.0 #scale depth
+        T = np.zeros(k1.shape+(len(self.isotherm_depths),))
+        moho = self.moho 
+        for i,z in enumerate(self.isotherm_depths):
+            if z<=moho:
+                exp = np.exp(-z/hr)
+                T[...,i] = self.T0 + qD*z/k1 + H0*hr**2/k1*(1-exp)
+            else:
+                exp1 = np.exp(-moho/hr)                
+                Tm = self.T0 + qD*moho/k1 + H0*hr**2/k1*(1-exp1)
+                T[...,i] = Tm + (z-moho)*qD/k2
+        return T
+
+
+
+#Test, runs when this script is executed individually
+if __name__ == '__main__':
+    print('Main')
+    forward = TempCalc(20e3,[10e3,30e3],0.0)
+      
+    fake_T = forward((2.5,3.0,110e-3,2.5e-6))
+    misfit = toy.IndependentGaussianMisfit(fake_T)
+    
+    prior = toy.RangePrior([2.0,2.5,0.0,0.0],[3.0,3.5,200e-3,3.0e-6])
+    proposal = toy.ComponentUpdateProposal([0.04,0.15,2e-3,2.0e-7])
+
+    hyperPrior = toy.RangePrior([1.0],[100.0])
+    hyperProposal = toy.ComponentUpdateProposal([0.5])
+    
+    x0 = np.array([2.75,3.25,100e-3,1.5e-6])
+
+    #Inversion with Markov-Chain-Monte-Carlo 
+    xchain,hyperChain,Lchain,accepted = toy.MCMC(x0,forward,misfit,prior,
+                                                 proposal,100000,
+                                                 np.array([5.0]),hyperProposal,
+                                                 hyperPrior)       
+    #Probabilities with which next model is taken
+    for i in range(4):
+        print (np.sum(np.abs((np.diff(xchain[:,i],axis=0)))>0)*8.0/100000.0)
+    print(accepted)
+    
+    #plots correlations and histograms
+    sns.pairplot(data=pd.DataFrame(xchain[50000:]))
+        
+    
---
+++ do_grid.py
@@ -0,0 +1,150 @@
+import numpy as np
+import wopy.MCMC.toy as toy
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+plt.ion()
+from Tpy import TempCalc,TempCalcExp
+
+#Whole Antarctic continent --> Default
+#Only Peninsula with heat production 
+#from Burton-Johnson et al. (2017) --> Peninsula
+what = 'Peninsula'
+model = 'AN1'
+
+if (what == 'Default'):
+    data = pd.read_csv('Models.csv', sep='\t')
+    p=4
+    print('Default')
+elif (what == 'Peninsula'): 
+    data = pd.read_csv('Peninsula_HP.csv', sep='\t')
+    A = data.HP*1e-6
+    p=3
+    print('Peninsula')
+    
+T0=0 #Surface Temperature
+N = len(data)
+Iterations = 1000
+all_means = np.zeros((N,p))
+all_std = np.zeros((N,p))
+all_accept = np.zeros((N,p)) 
+
+Temp = np.zeros((N,2))
+goal_T = np.array([580.0,1315.0])
+misfit = toy.IndependentGaussianMisfit(goal_T)
+hyper_mean = np.zeros(N)
+
+if (what=='Peninsula'):   
+    prior = toy.RangePrior([1.0,2.0,0.0],[3.0,4.0,200e-3]) 
+    proposal = toy.ComponentUpdateProposal([0.05,0.05,1e-3])
+else:
+    prior = toy.RangePrior([1.0,2.0,0.0,0.0],[3.0,4.0,200e-3,7.0e-6])
+    proposal = toy.ComponentUpdateProposal([0.05,0.05,1e-3,1.0e-7])
+
+hyperprior = toy.RangePrior([1.0],[10.0])
+hyperProposal = toy.ComponentUpdateProposal([0.5])
+
+for i in range(N):
+    curie = data.AN1_Curie[i]*1000
+    moho = data.AN1_Moho[i]*1000
+    lab = data.AN1_LAB[i]*1000
+
+    if (what == 'Peninsula'):
+        Aa = A[i]
+        forward = TempCalc(moho,[curie,lab],0.0,Aa) #or TempCalcExp
+        x0 = np.array([2.5,3.0,50e-3])
+    else: 
+        forward = TempCalc(moho,[curie,lab],0.0) #or TempCalcExp
+        x0 = np.array([2.5,3.0,50e-3,1.5e-6])
+
+    
+    t_0 = time.time()
+    xchain,hyperChain,Lchain,accepted = toy.MCMC(x0,forward,misfit,prior,
+                                                 proposal,Iterations,
+                                                 np.array([5.0]),hyperProposal,
+                                                 hyperprior)
+    t_1 = time.time()
+    Percentage = np.round(i*100/N,1)
+    print ('It took %.1f seconds' % (t_1-t_0),Percentage,'% of Task complete')
+    all_means[i,:] = xchain[500:,:].mean(0) 
+    all_std[i,:] = xchain[500:,:].std(0) 
+    hyper_mean[i] = hyperChain[500:].mean(0)
+    Temp[i,:] = forward(all_means[i,:])
+    print(Temp[i,0], Temp[i,1])
+       
+Tc = Temp[:,0]
+Tl = Temp[:,1]
+    
+    
+M = np.vstack((data.Lon,data.Lat,all_means[:,0:p].T,all_std[:,0:p].T,
+               Tc,Tl,hyper_mean)).T
+               
+for j in range(p):
+    all_accept[i,j]=np.sum(np.abs((np.diff(xchain[:,j],axis=0)))>0)*p*2.0/Iterations
+print ('Probability, with which next modelparameter is taken', all_accept[i,:])
+         
+np.savetxt('Results_%s_%s.txt' % (model, what), M, fmt="%.3e")    
+
+
+
+##-------------------------------Plot------------------------------------------
+
+x=data.Lon
+y=data.Lat
+k1=all_means[:,0]
+k2=all_means[:,1]
+qD=all_means[:,2]
+if (what=='Default'):
+    A=all_means[:,3]
+
+qS=1000*(qD+A*moho)
+print(qS[10])
+
+
+fig, axs = plt.subplots(5, sharex=True, sharey=True,figsize=(5,15))
+fig.suptitle('Parameters Inversion')
+plot1 = axs[0].scatter(x,y,c=k1,cmap='jet', marker=',', s=5)
+axs[0].set_title('Crustal Thermal Conductivity')
+cb1 = plt.colorbar(plot1,ax=axs[0])
+cb1.set_label('[W/mK]', labelpad=20, rotation=270) 
+
+plot2 = axs[1].scatter(x,y,c=k2,cmap='jet', marker=',', s=5)
+axs[1].set_title('Mantle Thermal Conductivity')
+cb2 = plt.colorbar(plot2,ax=axs[1])
+cb2.set_label('[W/mK]', labelpad=20, rotation=270) 
+
+plot3 = axs[2].scatter(x,y,c=qD*1000,cmap='jet', marker=',', s=5)
+axs[2].set_title('Mantle Heat Flux')
+cb3 = plt.colorbar(plot3,ax=axs[2])
+cb3.set_label('[mW/m$^2$]', labelpad=20, rotation=270) 
+
+plot4 = axs[3].scatter(x,y,c=A*1e6,cmap='jet', marker=',', s=5)
+axs[3].set_title('Heat Production')
+cb4 = plt.colorbar(plot4,ax=axs[3])
+cb4.set_label('[$\mu$Wm$^{-3}$]', labelpad=20, rotation=270) 
+
+plot5 = axs[4].scatter(x,y,c=qS,cmap='jet', marker=',', s=5, vmin=10, vmax=150)
+axs[4].set_title('Surface Heat Flux')
+cb5 = plt.colorbar(plot5,ax=axs[4])
+cb5.set_label('[mW/m$^2$]', labelpad=20, rotation=270) 
+
+fig.savefig('Parameter_%s_%s.png' % (model, what))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
